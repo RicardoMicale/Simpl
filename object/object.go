d840db1d789ed4234bdd01e593cfba81f5dde701
Ricardo Micale <ricardomicale@gmail.com>
2024-12-31 20:43:50 -0400

    objects and environment done

--- object/object.go
+++ object/object.go
@@ -11,9 +11,25 @@ const (
 	INTEGER_OBJECT = "INTEGER"
 	BOOLEAN_OBJECT = "BOOLEAN"
 	NULL_OBJECT = "NULL"
+	RETURN_VALUE_OBJECT = "RETURN_VALUE"
+	ERROR_OBJECT = "ERROR"
 )
 
 type Null struct {}
 
 func (n *Null) Type() ObjectType { return NULL_OBJECT }
 func (n *Null) Inspect() string { return "null" }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJECT }
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJECT }
+func (e *Error) Inspect() string { return "Error: " + e.Message }
